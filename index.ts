1deb3e03743c4e35de72986398d79c82cbf8adca
Your Name <romain.durand@radiofrance.com>
2023-10-11 15:55:45 +0200

    add details

--- index.ts
+++ index.ts
@@ -6,14 +6,29 @@ const fetsClient = createClient<NormalizedOAS>({
   endpoint: 'https://api.spotify.com/v1',
 });
 
-type Route = keyof typeof fetsClient;
+// the fets client itself is correctly typed, offers autocomplete + typechecking
+fetsClient['/playlists/{playlist_id}/tracks'].get({
+  headers: {
+    Authorization: `Bearer ${123}`,
+  },
+  params: {
+    playlist_id: '123',
+  }
+})
 
-async function callSpotifyApi(route: Route, method, params) {
+// but when I try to create a wrapper function, the params are not correctly typed
+async function callSpotifyApi<R extends Route>(route: R, method: Method<R>, params: Params<R, Method<R>>) {
   const result = await fetsClient[route][method]({
     ...params,
     headers: {
       Authorization: `Bearer ${123}`,
     },
-  });
+  } as any);
   return result;
 }
+
+// Route seems correctly typed
+type Route = keyof typeof fetsClient;
+// Method and Params are probably not
+type Method<R extends Route> = keyof typeof fetsClient[R];
+type Params<R extends Route, M extends Method<R>> = Omit<Parameters<typeof fetsClient[R][M]>[0], 'headers'>;
